8d484661a3a5e548e76fb66f839d806d7ed213f1
dacoder <107454678+dacoder101@users.noreply.github.com>
2023-02-24 23:40:31 +0000

    Fix?

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const form = document.getElementById("form")
 
+form.defineProperty("submit", 'defaultPrevented', {
+    value: "submit".defaultPrevented,
+    writable: true
+})
+
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
+    e.defaultPrevented = true
     var posInt = parseInt(document.getElementById("posIntSub").value)
     while (posInt != 1) {
         if (posInt % 2 == 0) {
