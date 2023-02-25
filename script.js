1bb3046ef2a8c6e5314fca98fb57c8e73e1e2e44
dacoder <107454678+dacoder101@users.noreply.github.com>
2023-02-25 00:30:33 +0000

    JS Onload

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
-var form = document.getElementById("form")
-var stepsElement = document.getElementById("steps")
-var sequenceElement = document.getElementById("sequence")
-var maxNumElement = document.getElementById("maxNum")
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    var posInt = parseInt(document.getElementById("posIntSub").value)
-    var sequence = [posInt]
-    var maxNum = posInt
-    var strSequence = ""
-
-    while (posInt != 1) {
-        if (posInt % 2 == 0) {
-        posInt /= 2
-        } else {
-        posInt *= 3
-        posInt += 1
+window.onload=function() {
+    var form = document.getElementById("form")
+    var stepsElement = document.getElementById("steps")
+    var sequenceElement = document.getElementById("sequence")
+    var maxNumElement = document.getElementById("maxNum")
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+        var posInt = parseInt(document.getElementById("posIntSub").value)
+        var sequence = [posInt]
+        var maxNum = posInt
+        var strSequence = ""
+    
+        while (posInt != 1) {
+            if (posInt % 2 == 0) {
+            posInt /= 2
+            } else {
+            posInt *= 3
+            posInt += 1
+            }
+    
+            sequence.push(posInt)
+            if (maxNum < posInt) {maxNum = posInt}
         }
-
-        sequence.push(posInt)
-        if (maxNum < posInt) {maxNum = posInt}
-    }
-
-    steps = sequence.length - 1
-    for (let i = 0; i < sequence.length; i++) {
-        strSequence += sequence[i] + ", "
-    }
-    strSequence = strSequence.slice(0, -2)
-
-    stepsElement.textContent = String(steps)
-    sequenceElement.textContent = strSequence
-    maxNumElement.textContent = String(maxNum)
-})
+    
+        steps = sequence.length - 1
+        for (let i = 0; i < sequence.length; i++) {
+            strSequence += sequence[i] + ", "
+        }
+        strSequence = strSequence.slice(0, -2)
+    
+        stepsElement.textContent = String(steps)
+        sequenceElement.textContent = strSequence
+        maxNumElement.textContent = String(maxNum)
+    })
+}
