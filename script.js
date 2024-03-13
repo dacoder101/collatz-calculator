3e16dfae3fb8dd700b1938045fd5828121c398e9
dacoder <bobbye23899@gmail.com>
2024-03-13 13:15:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,24 +11,27 @@ form.addEventListener("submit", (e) => {
     let maxNum = posInt;
     let strSequence = "";
 
-    while (posInt != 1) {
-        if (posInt % 2 == 0) {
-            posInt /= 2;
-        } else {
-            posInt *= 3;
-            posInt += 1;
+    if (posInt != null) {
+
+        while (posInt != 1) {
+            if (posInt % 2 == 0) {
+                posInt /= 2;
+            } else {
+                posInt *= 3;
+                posInt += 1;
+            }
+
+            sequence.push(posInt);
+            if (maxNum < posInt) {
+                maxNum = posInt;
+            }
         }
 
-        sequence.push(posInt);
-        if (maxNum < posInt) {
-            maxNum = posInt;
-        }
-    }
-
-    let steps = sequence.length - 1;
-    strSequence = sequence.join(", ");
+        let steps = sequence.length - 1;
+        strSequence = sequence.join(", ");
     
-    stepsElement.textContent = String(steps);
-    sequenceElement.textContent = strSequence;
-    maxNumElement.textContent = String(maxNum);
+        stepsElement.textContent = String(steps);
+        sequenceElement.textContent = strSequence;
+        maxNumElement.textContent = String(maxNum);
+    }
 });
